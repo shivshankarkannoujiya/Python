57ad592a6efcb60d15ac0edc53698c74f02bde95
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-09 22:28:58 +0530

    typeCasting

---
+++ Basics/07_typeCasting.py
@@ -0,0 +1,31 @@
+# 1
+# int -> string
+
+number = 10
+# converting
+numberString = str(number)
+# print(type(number))
+# print(type(numberString))
+
+# 2
+# @Also can convert list -> string
+list = [1, 2, 3, 4, 5]
+# converting
+listString = str(list)
+# print(type(list))
+# print(type(listString))
+
+# 3
+# string -> integer
+strNum = "10"
+# converting
+intNum = int(strNum)
+# print(type(strNum))
+# print(type(intNum))
+
+
+# 4 int -> float
+integrNum = 10
+floatNum = float(intNum)
+# print(type(integrNum))
+# print(type(floatNum))
