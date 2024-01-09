dda8d41a0f5dedbe94df7db94c1f507462d5ce69
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-09 18:50:47 +0530

    dataTypes

---
+++ Basics/02_datatypes.py
@@ -0,0 +1,27 @@
+a = "Abhishek"
+b = 10
+c = 10.1
+e = []
+f = {}
+g = ()
+h = 5+7j
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+
+# Output
+"""
+<class 'str'>
+<class 'int'>
+<class 'float'>
+<class 'list'>
+<class 'dict'>
+<class 'tuple'>
+<class 'complex'>
+
+"""
