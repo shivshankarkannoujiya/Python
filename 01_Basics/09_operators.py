43f2ebdfcba5cf8de8ff998928adf1e8062779c5
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-10 14:30:02 +0530

    Operators

---
+++ 01_Basics/09_operators.py
@@ -0,0 +1,87 @@
+# Boolean
+
+# @True
+# print(type(True))
+# print(bool(1))
+# print(bool("abc"))
+# print(bool([1, 2, 3]))
+
+# @False
+# print(type(False))
+# print(bool(0))
+# print(bool(" "))
+# print(bool([]))
+
+
+# Operators
+# a = 10
+# b = 20
+
+# 1.
+# @Arithematic Operator
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a**b)
+# print(a % b)
+# print(a // b)
+# print(a / b)
+
+
+# 2
+# @Assignment Operator
+
+# a += 5
+# b *= 5
+# print(a)
+# print(b)
+
+
+# 3
+# @Comparision Operator : return -> true/false
+
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a >= b)
+# print(a < b)
+# print(a <= b)
+
+
+# 4
+# @Logical Operator
+# and , or not
+
+# a = 2
+# b = 25
+# print(a > 12 and a < 25)
+# print(a > 12 or a < 25)
+# print(not (a > 25))
+
+
+# 5
+# @Identity Operator
+# is : check [value+memoryAddress]
+# == : check only Value
+# a = 10 -> true <point same memory Address>
+# b = 10 -> true <point same memory Address>
+
+# a = []
+# b = []
+# print(a is b)  # false <Memory address is not same>
+# print(a == b)  # true <Checks only value >
+
+
+# 6
+# @Membership Operators
+# present or not : [in]
+# not in
+name = "My name is Abhishek"
+# print("Abhi" in name)
+# print("Abhish" in name)
+# print("Abhishek" in name)
+
+
+# not in
+# print(not "Abhi" in name)
