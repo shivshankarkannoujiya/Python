f670e3ad9bebe68116709112d716f871bd01688d
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-13 13:50:58 +0530

    updated

--- 04__Project_!/main.py
+++
@@ -1,8 +0,0 @@
-from bardapi import Bard
-import os
-
-token = 'eQjsdwMgYEwJdfRhHrkXudjgSiWi1Uso1197ajiH7YpHfQ11lU68lXY-jRVnSgRXxG_6kw.'
-os.environ["_BARD_API_KEY"]=token
-
-message = input("Enter your Prompt : ")
-print(Bard().get_answer(str(message)))
